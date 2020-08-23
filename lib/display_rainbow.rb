53188845947c015410ec8f2ed694343bb15528f7
Yiliang Chen <yiliangchen0113@hotmail.com>
2020-08-23 18:06:27 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,10 +1,9 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors)
+  colors = array.new[ "R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
   a = 0 
   if (a<=7) {
     puts colors[a]
     a++
   }
 end
-
-colors = array.new[ "R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
