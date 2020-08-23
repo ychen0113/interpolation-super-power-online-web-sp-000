10b89573f8204d80c49d578da3e5f00112d33d0d
Yiliang Chen <yiliangchen0113@hotmail.com>
2020-08-23 18:12:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow
-  colors = Array.new[ "R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]
+def display_rainbow(colors = Array.new[ "R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"])
   puts colors[0]
   puts colors[1]
   puts colors[2]
